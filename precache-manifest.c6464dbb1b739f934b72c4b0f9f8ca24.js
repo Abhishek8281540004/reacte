0058840e14bce268dc96ef362ac9c4ffd21b8448
Abhishek <abhishekravi063@gmail.com>
2023-04-18 14:21:50 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.c6464dbb1b739f934b72c4b0f9f8ca24.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "55e0b7fa842928fdb473",
+    "url": "/react-gh-pages/static/css/main.eeeee64b.chunk.css"
+  },
+  {
+    "revision": "55e0b7fa842928fdb473",
+    "url": "/react-gh-pages/static/js/main.55e0b7fa.chunk.js"
+  },
+  {
+    "revision": "9c5937448187a6151b43",
+    "url": "/react-gh-pages/static/js/1.9c593744.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "68777e9738cf44a28fe3db67e269f224",
+    "url": "/react-gh-pages/static/media/javascript.68777e97.png"
+  },
+  {
+    "revision": "cf71a98c6bc707f2d070388c7afcb0c9",
+    "url": "/react-gh-pages/static/media/css.cf71a98c.png"
+  },
+  {
+    "revision": "80683bb1038b7b7ef66b2160dfa45416",
+    "url": "/react-gh-pages/static/media/firebase.80683bb1.png"
+  },
+  {
+    "revision": "0ceb738c2705c44af69145c5ec4beebf",
+    "url": "/react-gh-pages/static/media/github.0ceb738c.png"
+  },
+  {
+    "revision": "4d88ca033fd854136a7695046e8910d4",
+    "url": "/react-gh-pages/static/media/tailwind.4d88ca03.png"
+  },
+  {
+    "revision": "8c1a6705c0aa95cd62fa1d379da70dae",
+    "url": "/react-gh-pages/static/media/mongo.8c1a6705.png"
+  },
+  {
+    "revision": "4380e99d266b735f5a05be5b9cd8ccdb",
+    "url": "/react-gh-pages/static/media/node.4380e99d.png"
+  },
+  {
+    "revision": "a80a90015e4a63d3cf6a71875f27937e",
+    "url": "/react-gh-pages/static/media/html.a80a9001.png"
+  },
+  {
+    "revision": "1b28808441932b7571626b4edcc307fe",
+    "url": "/react-gh-pages/static/media/react.1b288084.png"
+  },
+  {
+    "revision": "ad9906e7f4a17fa88140da1d0cf07373",
+    "url": "/react-gh-pages/static/media/Netflix.ad9906e7.png"
+  },
+  {
+    "revision": "33c56d4ce9af6f301d83d509eae6cd1c",
+    "url": "/react-gh-pages/static/media/Fitclub.33c56d4c.png"
+  },
+  {
+    "revision": "ebd92a04e381ed4ebd9a375f5d6075c1",
+    "url": "/react-gh-pages/static/media/Plante.ebd92a04.png"
+  },
+  {
+    "revision": "3441cb26c07528d60f80eb9ffd5c84cc",
+    "url": "/react-gh-pages/static/media/porfolio-html.3441cb26.png"
+  },
+  {
+    "revision": "fc0bdd0cbb8bb87a0e63d21f19f1ecfd",
+    "url": "/react-gh-pages/static/media/castle.fc0bdd0c.jpg"
+  },
+  {
+    "revision": "e5014dfcf5675cd7ed023668c7690318",
+    "url": "/react-gh-pages/static/media/or.e5014dfc.jpg"
+  },
+  {
+    "revision": "2247030c946ca53f7cc6bbbdbeb2b4ff",
+    "url": "/react-gh-pages/static/media/blue.2247030c.jpg"
+  },
+  {
+    "revision": "d7eb9778669567a16ecca34d501e0086",
+    "url": "/react-gh-pages/static/media/mou.d7eb9778.jpg"
+  },
+  {
+    "revision": "ccc99e6f9e0a728c48ff61641146f55a",
+    "url": "/react-gh-pages/static/media/redmou.ccc99e6f.jpg"
+  },
+  {
+    "revision": "3c9f8e527fe79efda708a354fcb8ba32",
+    "url": "/react-gh-pages/index.html"
+  }
+];
